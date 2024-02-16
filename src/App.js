7333fad292a5d4c11644caff0af0245f3f9af1b3
ritikj27 <ritik.j@preciousinfosystem.com>
2024-02-16 14:32:04 +0530

    fix issue

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
-// import React, { useState } from 'react';
-// import { QrReader } from 'react-qr-reader';
+import React, { useState } from 'react';
+import { QrReader } from 'react-qr-reader';
 
-// const App = (props) => {
-//   const [data, setData] = useState('No result');
+const App = (props) => {
+  const [data, setData] = useState('No result');
 
-//   return (
-//     <>
-//       <QrReader
-//          scanDelay={300}
-//          constraints={{
-//            aspectRatio: "1",
-//            facingMode: {
-//              exact: "environment"
-//            },
-//            width: { min: 480, ideal: 720, max: 1280 },
-//          }}
-//         onResult={(result, error) => {
-//           if (!!result) {
-//             setData(result?.text);
-//           }
+  return (
+    <>
+      <QrReader
+         scanDelay={300}
+         constraints={{
+           aspectRatio: "1",
+           facingMode: {
+             exact: "environment"
+           },
+           width: { min: 480, ideal: 720, max: 1280 },
+         }}
+        onResult={(result, error) => {
+          if (!!result) {
+            setData(result?.text);
+          }
 
-//           if (!!error) {
-//             console.info(error);
-//           }
-//         }}
-//         style={{ width: '100%' }}
-//       />
-//       <p>{data}</p>
-//     </>
-//   );
-// };
-// export default App
+          if (!!error) {
+            console.info(error);
+          }
+        }}
+        style={{ width: '100%' }}
+      />
+      <p>{data}</p>
+    </>
+  );
+};
+export default App
 
 
 
@@ -101,56 +101,56 @@
 // export default App;
 
 
-import React, { useState } from 'react';
-import { QrReader } from 'react-qr-reader';
+// import React, { useState } from 'react';
+// import { QrReader } from 'react-qr-reader';
 
-const App = (props) => {
-  const [scannedData, setScannedData] = useState('');
-  const [part, setPart] = useState([]);
-  const [isScanModal, setIsScanModal] = useState(false);
+// const App = (props) => {
+//   const [scannedData, setScannedData] = useState('');
+//   const [part, setPart] = useState([]);
+//   const [isScanModal, setIsScanModal] = useState(false);
 
-  const handleScan = (result) => {
-    if (result) {
-      const scannedText = result?.text; // Extracting the scanned text from the result object
-      setScannedData(scannedText);
-      if (part.includes(scannedText)) {
-        alert(`Duplicate QR code. Please scan a unique QR code.`);
-      } else {
-        setScannedData(scannedText);
-        setPart([...part, scannedText]);
-        setIsScanModal(!isScanModal);
-      }
-    }
-  };
+//   const handleScan = (result) => {
+//     if (result) {
+//       const scannedText = result?.text; // Extracting the scanned text from the result object
+//       setScannedData(scannedText);
+//       if (part.includes(scannedText)) {
+//         alert(`Duplicate QR code. Please scan a unique QR code.`);
+//       } else {
+//         setScannedData(scannedText);
+//         setPart([...part, scannedText]);
+//         setIsScanModal(!isScanModal);
+//       }
+//     }
+//   };
 
-  const handleError = (error) => {
-    console.error(error);
-  };
+//   const handleError = (error) => {
+//     console.error(error);
+//   };
 
-  return (
-    <>
-    <h1>test 1</h1>
-      <QrReader
-        scanDelay={300}
-        constraints={{
-          aspectRatio: "1",
-          facingMode: {
-            exact: "environment"
-          },
-          width: { min: 480, ideal: 720, max: 1280 },
-        }}
-        onError={handleError}
-        onScan={handleScan}
-        style={{ width: '100%' }}
-      />
-      <p>Scanned Data: {scannedData}</p>
-      <ul>
-        {part.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-    </>
-  );
-};
+//   return (
+//     <>
+//     <h1>test 1</h1>
+//       <QrReader
+//         scanDelay={300}
+//         constraints={{
+//           aspectRatio: "1",
+//           facingMode: {
+//             exact: "environment"
+//           },
+//           width: { min: 480, ideal: 720, max: 1280 },
+//         }}
+//         onError={handleError}
+//         onScan={handleScan}
+//         style={{ width: '100%' }}
+//       />
+//       <p>Scanned Data: {scannedData}</p>
+//       <ul>
+//         {part.map((item, index) => (
+//           <li key={index}>{item}</li>
+//         ))}
+//       </ul>
+//     </>
+//   );
+// };
 
-export default App;
+// export default App;
