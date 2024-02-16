f9ab7676517b78741f0f3f6901b3118e01485374
ritikj27 <ritik.j@preciousinfosystem.com>
2024-02-16 13:05:47 +0530

    fix issue

--- src/App.js
+++ src/App.js
@@ -3,28 +3,13 @@ import { QrReader } from "react-qr-reader";
 
 const App = (props) => {
   const [data, setData] = useState("No result");
-  const [part,setPart] = useState();
-  const [setIsScanModal,isScanModal] = useState()
-  const [FieldsValue,setFieldsValue] = useState()
+  const [part, setPart] = useState([]);
+  const [isScanModal, setIsScanModal] = useState(false);
+  const [fieldsValue, setFieldsValue] = useState({ partId: "" });
 
   return (
     <>
       <h2>hello world</h2>
-      {/* <QrReader scanDelay={300} constraints={{
-        facingMode: "user"
-      }}
-        onResult={(result, error) => {
-          if (result) {
-            setData(result?.getText());
-          }
-
-          if (error) {
-            console.info(error);
-          }
-        }}
-        style={{ width: '100%' }}
-      />
-      <p>{data}</p> */}
       <QrReader
         id="qrReader"
         scanDelay={300}
@@ -36,29 +21,26 @@ const App = (props) => {
           width: { min: 480, ideal: 720, max: 1280 },
         }}
         onError={(error) => console.error(error)}
-        onResult={(result) => {
+        onScan={(result) => {
           if (result) {
-            const readerRef = document.getElementById("qrReader");
-            if (readerRef) {
-              readerRef.autofocus();
-              const scannedText = result.getText();
-              if (part.includes(scannedText)) {
-                alert(`Duplicate QR code. Please scan a unique QR code.`);
-              } else {
-                setPart([...part, scannedText]);
-               setFieldsValue({
-                  partId: scannedText,
-                });
-                setIsScanModal(!isScanModal);
-              }
+            const scannedText = result;
+            if (part.includes(scannedText)) {
+              alert(`Duplicate QR code. Please scan a unique QR code.`);
+            } else {
+              setPart([...part, scannedText]);
+              setFieldsValue({
+                partId: scannedText,
+              });
+              setIsScanModal(!isScanModal);
             }
           }
         }}
         style={{ width: "100%" }}
       />
-
-      <h2>{FieldsValue?.partId}</h2>
+      <h2>{fieldsValue.partId}</h2>
     </>
   );
 };
+
 export default App;
+
